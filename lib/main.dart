a7216853ea72c6533e86b5a218337e3166ed25e4
RuslanTsitser <cicer_ruslan@mail.ru>
2023-10-01 12:43:20 +0300

    main flow

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spain_bot_web_app/features/bls_spain/data/bls_spain_repository.dart';
+import 'package:spain_bot_web_app/features/bls_spain/presentation/bls_page.dart';
+import 'package:spain_bot_web_app/features/splash/error_page.dart';
+import 'package:spain_bot_web_app/features/splash/splash_page.dart';
+import 'package:spain_bot_web_app/injection.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+import 'core/services/export.dart';
 
-class MainApp extends StatelessWidget {
+void main() => runApp(
+      InjectionServiceWidget(
+        firebaseService: FirebaseServiceImpl(),
+        firebaseAuthService: FirebaseAuthServiceImpl(),
+        firestoreService: FirestoreServiceImpl(),
+        telegramService: TelegramServiceImpl(),
+        urlLauncherService: UrlLauncherServiceImpl(),
+        child: const MainApp(),
+      ),
+    );
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late Future<void> _future;
+
+  @override
+  void initState() {
+    _future = InjectionServiceWidget.of(context).initServices();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      home: FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashPage();
+          } else if (snapshot.hasError) {
+            return const ErrorPage();
+          }
+
+          return Provider<BlsSpainRepository>(
+            create: (context) => BlsSpainRepositoryImpl(
+              firestoreService: InjectionServiceWidget.of(context).firestoreService,
+              telegramService: InjectionServiceWidget.of(context).telegramService,
+              urlLauncherService: InjectionServiceWidget.of(context).urlLauncherService,
+            ),
+            child: const BlsPage(),
+          );
+        },
       ),
     );
   }
